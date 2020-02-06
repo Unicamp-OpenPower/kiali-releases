a1bac099c189b785075e3f96330fe8e2e7dd5e5c
Junior Santos <59625793+jr-santos98@users.noreply.github.com>
2020-02-06 16:30:41 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -22,16 +22,16 @@ then
     mv /home/travis/gopath/src/github.com/Unicamp-OpenPower/kiali-releases/Makefile $GOPATH/src/github.com/kiali/kiali/Makefile
     make build
     cd $GOPATH/bin
-    ls
-    #mv kiali kiali-$github_version
     #ls
-    #./kiali-$github_version -help
+    mv kiali kiali-$github_version
+    ls
+    ./kiali-$github_version -help
     
     #if [[ $github_version > $ftp_version ]]
     #then
         #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/kiali/latest kiali-$github_version"
         #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/kiali/latest/kiali-$ftp_version" 
     #fi
-    #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/kiali kiali-$github_version"
+    lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/kiali kiali-$github_version"
     #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/kiali/kiali-$del_version" 
 fi
