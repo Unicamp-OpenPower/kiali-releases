9bac54de2fe012587068f2e7a41f2541e52f8460
Junior Santos <59625793+jr-santos98@users.noreply.github.com>
2020-02-06 17:14:46 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 github_version=$(cat github_version.txt)
-#ftp_version=$(cat ftp_version.txt)
-ftp_version=1.2.0
+ftp_version=$(cat ftp_version.txt)
 del_version=$(cat delete_version.txt)
 
 if [ $github_version != $ftp_version ]
@@ -26,11 +25,11 @@ then
     ls
     ./kiali-$github_version -help
     
-    #if [[ $github_version > $ftp_version ]]
-    #then
-        #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/kiali/latest kiali-$github_version"
-        #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/kiali/latest/kiali-$ftp_version" 
-    #fi
+    if [[ $github_version > $ftp_version ]]
+    then
+        lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/kiali/latest kiali-$github_version"
+        lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/kiali/latest/kiali-$ftp_version" 
+    fi
     lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/kiali kiali-$github_version"
-    #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/kiali/kiali-$del_version" 
+    lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/kiali/kiali-$del_version" 
 fi
