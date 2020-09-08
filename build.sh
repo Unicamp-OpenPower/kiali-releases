8e3997eec4663ea4782f8a3d707028bc98221da8
Junior Santos <claudio.dossantosjunior@yahoo.com.br>
2020-09-08 19:30:14 -0300

    Update release

--- build.sh
+++ build.sh
@@ -1,35 +1,30 @@
 github_version=$(cat github_version.txt)
 ftp_version=$(cat ftp_version.txt)
 del_version=$(cat delete_version.txt)
+github_version=1.18.3
 
 if [ $github_version != $ftp_version ]
 then
-    wget https://oplab9.parqtec.unicamp.br/pub/ppc64el/glide/glide-0.13.3
-    mv glide-0.13.3 glide
-    sudo chmod 755 glide
-    sudo mv glide /usr/bin/glide
-  
     cd $GOPATH/src/github.com
     mkdir kiali
     cd kiali
     wget https://github.com/kiali/kiali/archive/v$github_version.zip
     unzip v$github_version.zip
     mv kiali-$github_version kiali
-    cd kiali 
-    rm Makefile
-    mv /home/travis/gopath/src/github.com/Unicamp-OpenPower/kiali-releases/Makefile $GOPATH/src/github.com/kiali/kiali/Makefile
+    git clone https://github.com/kiali/kiali-operator.git kiali/operator
+    git clone https://github.com/kiali/kiali-operator.git kiali/helm-charts
+    cd kiali
     make build
     cd $GOPATH/bin
-    #ls
     mv kiali kiali-$github_version
     ls
     ./kiali-$github_version -help
-    
-    if [[ $github_version > $ftp_version ]]
+
+    if [[ $github_version != $ftp_version ]]
     then
         lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/kiali/latest kiali-$github_version"
-        lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/kiali/latest/kiali-$ftp_version" 
+        #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/kiali/latest/kiali-$ftp_version"
     fi
     lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /ppc64el/kiali kiali-$github_version"
-    lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/kiali/kiali-$del_version" 
+    #lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; rm /ppc64el/kiali/kiali-$del_version"
 fi
---
+++ release.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+github_version=$(cat github_version.txt)
+ftp_version=$(cat ftp_version.txt)
+github_version=1.18.3
+LOCALPATH=$GOPATH/src/github.com/kiali/kiali
+BINPATH=$GOPATH/bin
+
+if [ $github_version != $ftp_version ]
+then
+  git clone https://$USERNAME:$TOKEN@github.com/Unicamp-OpenPower/repository-scrips.git
+  cd repository-scrips/
+  chmod +x empacotar-deb.sh
+  chmod +x empacotar-rpm.sh
+  sudo mv empacotar-deb.sh $BINPATH
+  sudo mv empacotar-rpm.sh $BINPATH
+  cd $BINPATH
+  sudo ./empacotar-deb.sh kiali kiali-$github_version $github_version " "
+  sudo ./empacotar-rpm.sh kiali kiali-$github_version $github_version " " "Kiali project, observability for the Istio service mesh"
+  if [ $github_version > $ftp_version ]
+  then
+    lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /repository/debian/ppc64el/kiali/ $BINPATH/kiali-$github_version-ppc64le.deb"
+    sudo lftp -c "open -u $USER,$PASS ftp://oplab9.parqtec.unicamp.br; put -O /repository/rpm/ppc64le/kiali/ ~/rpmbuild/RPMS/ppc64le/kiali-$github_version-1.ppc64le.rpm"
+  fi
+fi
